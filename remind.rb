947b97896a24631871f89abc5c4e036a3e4e1d2f
Yusuke Nakamura <yusuke1994525@gmail.com>
2019-03-20 10:00:21 +0900

    Creeate remind.rb to remind participating github notifications

---
+++ remind.rb
@@ -0,0 +1,27 @@
+require 'json'
+require 'octokit'
+require 'slack-ruby-client'
+
+github_client = Octokit::Client.new(access_token: ENV["GITHUB_API_TOKEN"])
+slack_client = Slack::Web::Client.new(token: ENV["SLACK_API_TOKEN"])
+
+notifications = github_client.notifications(participating: true)
+
+attachments = notifications.map do |n|
+  {
+    color: '#36a64f',
+    auther_name: n.repository.full_name,
+    auther_link: n.repository.html_url,
+    title: n.subject.title,
+    title_link: n.subject.url,
+    footer: n.repository.private ? ':lock:' : ':earth_asia:',
+    ts: n.updated_at.to_i,
+  }
+end
+
+message = {
+  text: 'Unread notifications',
+  attachments: attachments
+}
+
+slack_client.chat_postMessage(channel: ENV['SLACK_CHANNEL'], as_user: true, **message)
